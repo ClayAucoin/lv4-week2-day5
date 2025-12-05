0bc70498d702ef8acc792017f77e501ead358ce3
ClayAucoin <clay0aucoin@gmail.com>
2025-12-05 16:18:33 -0600

    added post item successfully

--- src/app.js
+++ src/app.js
@@ -41,5 +41,37 @@ app.get("/items", async (req, res, next) => {
   })
 })
 
+// add item
+app.post("/items",
+  requireBody,
+  validateItemBody,
+  validateAllowedFields,
+  async (req, res, next) => {
+    const newItem = req.body
+    console.log("POST /items", newItem)
+
+    const { data, error } = await supabase
+      .from("movies_simple")
+      .insert(newItem)
+      .select()
+      .maybeSingle()
+
+    if (error) {
+      return next(sendError(
+        500,
+        "Failed to add item",
+        "INSERT_ERROR",
+        { underlying: error.message }
+      ))
+    }
+
+    res.status(201).json({
+      ok: true,
+      records: 1,
+      message: "Item added successfully",
+      data: data
+    })
+  })
+
 
 export default app
