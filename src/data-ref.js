97324f9da754e83aa9088899e531fe1193a86166
ClayAucoin <clay0aucoin@gmail.com>
2025-12-05 19:28:37 -0600

    added data ref

---
+++ src/data-ref.js
@@ -0,0 +1,15 @@
+const dataShape = {
+  "imdb_id": "tt4172430",
+  "title": "13 Hours: The Secret Soldiers of Benghazi",
+  "year": 2016,
+  "runtime": "2:24:29",
+  "rating": "R",
+  "poster": "https://image.tmdb.org/t/p/original/AskDcQ6Sa6jImyt2KDQbgRuPebH.jpg",
+  "genres": [
+    "War",
+    "Action",
+    "History",
+    "Drama",
+    "Thriller"
+  ]
+}
